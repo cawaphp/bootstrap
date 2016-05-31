ba52df60b15482be6a98331dc5869e0a21e25fad
tchiotludo <tchiot.ludo@gmail.com>
2016-05-31 12:12:13 +0200

    ImageField : sortableUrl

--- assets/forms/fields/image.js
+++ assets/forms/fields/image.js
@@ -8,7 +8,9 @@ $.widget("cawa.fields-image", $.cawa.widget, {
             language: $.locale(),
             showUpload: false,
             showClose: false,
-            showDelete: true
+            fileActionSettings: {
+
+            }
         }
     },
 
@@ -24,6 +26,7 @@ $.widget("cawa.fields-image", $.cawa.widget, {
                 if (typeof value == "string") {
                     pluginOptions.initialPreview.push('<img src="' + value + '" class="file-preview-image" />');
                 } else {
+
                     pluginOptions.initialPreview.push('<img src="' + key + '" class="file-preview-image" />');
                     pluginOptions.initialPreviewConfig.push(value);
                 }
@@ -34,12 +37,43 @@ $.widget("cawa.fields-image", $.cawa.widget, {
             pluginOptions.overwriteInitial = false;
         }
 
+
+        if (this.options.deleteUrl) {
+            pluginOptions.deleteUrl = this.options.deleteUrl;
+        } else {
+            pluginOptions.initialPreviewShowDelete = false;
+        }
+
+        if (!this.options.sortUrl) {
+            pluginOptions.fileActionSettings.showDrag = false;
+        }
+
         this.options.required = this.element.prop("required");
         this.element.prop("required", false);
         this.element.attr("data-rule-image", "true");
 
         /* Plugins */
         this.element.fileinput(pluginOptions);
+
+        if (this.options.sortUrl) {
+            this.element.on('filesorted', $.proxy(this._onSorted, this));
+        }
+    },
+
+    _onSorted: function(event, params)
+    {
+        var data = {
+            extra: params.stack[params.newIndex].extra,
+            oldIndex: params.oldIndex,
+            newIndex: params.newIndex
+        };
+
+        $.ajax({
+            url: this.options.sortUrl,
+            type: 'POST',
+            dataType: "json",
+            data: data
+        });
     },
 
     isValid: function()
