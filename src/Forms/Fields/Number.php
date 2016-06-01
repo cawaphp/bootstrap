6893a5876693d9ce3349c252439b5af446a95ef9
tchiotludo <tchiot.ludo@gmail.com>
2016-06-01 07:19:51 +0200

    NumberField

---
+++ src/Forms/Fields/Number.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Cawa\Bootstrap\Forms\Fields;
+
+class Number extends \Cawa\Html\Forms\Fields\Number
+{
+    use FieldTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(string $name, string $label = null)
+    {
+        parent::__construct($name, $label);
+        $this->getField()->addClass('form-control');
+        $this->addClass('form-group');
+        $this->setValidation('int');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render()
+    {
+        return $this->renderBootstrapProperties();
+    }
+}
