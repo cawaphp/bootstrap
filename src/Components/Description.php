3dee775e10006bb9649cba507e602e1b4b1d9bbb
tchiotludo <tchiot.ludo@gmail.com>
2016-06-09 16:58:05 +0200

    Components Description

---
+++ src/Components/Description.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Cawa\Bootstrap\Components;
+
+use Cawa\Html\Link;
+use Cawa\Renderer\HtmlContainer;
+use Cawa\Renderer\HtmlElement;
+
+class Description extends HtmlContainer
+{
+    /**
+     * @param array $keyValue
+     */
+    public function __construct(array $keyValue = [])
+    {
+        parent::__construct('<dl>');
+
+        foreach ($keyValue as $key => $value) {
+            $this->add((new HtmlElement('<dt>', $key)));
+            if ($value) {
+                $this->add((new HtmlElement('<dd>', $value)));
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHorizontal() : bool
+    {
+        return $this->hasClass('dl-horizontal');
+    }
+
+    /**
+     * @param bool $horizontal
+     *
+     * @return $this
+     */
+    public function setHorizontal(bool $horizontal = true)
+    {
+        if ($horizontal) {
+            $this->addClass('dl-horizontal');
+        } else {
+            $this->removeClass('dl-horizontal');
+        }
+
+        return $this;
+    }
+
+}
