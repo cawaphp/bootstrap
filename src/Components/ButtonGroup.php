56991337417a5a7b6a54f360240da16f4b1d5f40
tchiotludo <tchiot.ludo@gmail.com>
2016-07-01 12:04:27 +0200

    Panel Components

---
+++ src/Components/ButtonGroup.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace Cawa\Bootstrap\Components;
+
+use Cawa\Renderer\HtmlContainer;
+
+class ButtonGroup extends HtmlContainer
+{
+    const SIZE_LARGE = 'btn-group-lg';
+    const SIZE_SMALL = 'btn-group-sm';
+    const SIZE_XSMALL = 'btn-group-xs';
+
+    /**
+     * @param string $size
+     */
+    public function __construct(string $size = null) {
+        parent::__construct('<div>');
+
+        $this->addClass('btn-group');
+        $this->addAttribute('role', 'group');
+
+        if ($size) {
+            $this->addClass($size);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isJustified() : bool
+    {
+        return $this->hasClass('btn-group-justified');
+    }
+
+    /**
+     * @param bool $justified
+     *
+     * @return $this
+     */
+    public function setJustified(bool $justified = true) : self
+    {
+        if ($justified) {
+            $this->addClass('btn-group-justified');
+        } else {
+            $this->removeClass('btn-group-justified');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVertical() : bool
+    {
+        return $this->hasClass('btn-group-vertical');
+    }
+
+    /**
+     * @param bool $vertical
+     *
+     * @return $this
+     */
+    public function setVertical(bool $vertical = true) : self
+    {
+        if ($vertical) {
+            $this->addClass('btn-group-vertical')
+                ->removeClass('btn-group');
+
+        } else {
+            $this->removeClass('btn-group-vertical')
+                ->addClass('btn-group');
+        }
+
+        return $this;
+    }
+}
---
+++ src/Components/Panel.php
@@ -0,0 +1,197 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Cawa\Bootstrap\Components;
+
+use Cawa\Bootstrap\Tables\Grid;
+use Cawa\Bootstrap\Tables\Table;
+use Cawa\Controller\ViewController;
+use Cawa\Renderer\Container;
+use Cawa\Renderer\Element;
+use Cawa\Renderer\HtmlContainer;
+use Cawa\Renderer\HtmlElement;
+
+class Panel extends HtmlContainer
+{
+    const TYPE_DEFAULT = 'panel-default';
+    const TYPE_PRIMARY = 'panel-primary';
+    const TYPE_SUCCESS = 'panel-success';
+    const TYPE_INFO = 'panel-info';
+    const TYPE_WARNING = 'panel-warning';
+    const TYPE_DANGER = 'panel-danger';
+
+    /**
+     * @param string $type
+     * @param string $title
+     * @param string $content
+     */
+    public function __construct(string $type = self::TYPE_DEFAULT, string $title = null, string $content = null)
+    {
+        parent::__construct('<div>', $content);
+        $this->addClass(['panel', $type]);
+        $this->container = new Container();
+        $this->title = $title;
+    }
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @return string
+     */
+    public function getTitle() : string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle(string $title) : self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @var string
+     */
+    private $footer;
+
+    /**
+     * @return string
+     */
+    public function getFooter() : string
+    {
+        return $this->footer;
+    }
+
+    /**
+     * @param string $footer
+     *
+     * @return $this
+     */
+    public function setFooter(string $footer) : self
+    {
+        $this->footer = $footer;
+
+        return $this;
+    }
+
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(ViewController ...$elements)
+    {
+        $this->container->add(...$elements);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFirst(ViewController ...$elements)
+    {
+        $this->container->addFirst(...$elements);
+
+        return $this;
+    }
+
+    /**
+     * @var ButtonGroup
+     */
+    private $buttonGroup;
+
+
+    public function addButton(Button $button)
+    {
+        if (!$this->buttonGroup) {
+            $this->buttonGroup = (new ButtonGroup(ButtonGroup::SIZE_SMALL))
+                ->addClass('pull-right');
+        }
+
+
+        $this->buttonGroup->add($button);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render()
+    {
+        if ($this->title || $this->title) {
+            $header = (new HtmlContainer('<div>'))
+                ->addClass('panel-heading');
+
+            if ($this->buttonGroup) {
+                $header->add($this->buttonGroup);
+            }
+
+            if ($this->title) {
+                $header->add((new HtmlElement('<h3>', $this->title))->addClass('panel-title'));
+            }
+
+            parent::add($header);
+        }
+
+        if ($this->content) {
+            parent::add((new HtmlElement('<div>', $this->content))
+                ->addClass('panel-body')
+            );
+        } else {
+            $container = new Container();
+            foreach ($this->container->getElements() as $element) {
+                if ($element instanceof Table || $element instanceof Grid || $element instanceof ListGroup) {
+                    if (sizeof($container->getElements()) > 0) {
+                        parent::add((new HtmlContainer('<div>'))
+                            ->addClass('panel-body')
+                            ->add($container)
+                        );
+                    }
+
+                    parent::add($element);
+                } else {
+                    $container->add($element);
+                }
+
+                if (sizeof($container->getElements()) > 0) {
+                    parent::add((new HtmlContainer('<div>'))
+                        ->addClass('panel-body')
+                        ->add($container)
+                    );
+                }
+            }
+        }
+
+        if ($this->footer) {
+            parent::add((new HtmlElement('<div>', $this->footer))
+                ->addClass('panel-footer')
+            );
+        }
+
+        return parent::render();
+    }
+}
