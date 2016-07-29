c5c6fc784ea8a084919f5d98e844ee34de86ad42
tchiotludo <tchiot.ludo@gmail.com>
2016-07-29 15:56:57 +0200

    Submit::setType

--- src/Forms/Fields/Submit.php
+++ src/Forms/Fields/Submit.php
@@ -22,6 +22,13 @@ class Submit extends \Cawa\Html\Forms\Fields\Submit
         FieldTrait::layout as private fieldTraitLayout;
     }
 
+    const TYPE_DEFAULT = 'btn-default';
+    const TYPE_PRIMARY = 'btn-primary';
+    const TYPE_SUCCESS = 'btn-success';
+    const TYPE_INFO = 'btn-info';
+    const TYPE_WARNING = 'btn-warning';
+    const TYPE_DANGER = 'btn-danger';
+
     /**
      * {@inheritdoc}
      */
@@ -30,6 +37,25 @@ class Submit extends \Cawa\Html\Forms\Fields\Submit
         parent::__construct($value);
         $this->getField()->addClass(['btn', 'btn-primary']);
         $this->addClass('form-group');
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return Submit
+     */
+    public function setType(string $type) : self
+    {
+        $reflection = new \ReflectionClass($this);
+        foreach ($reflection->getConstants() as $key => $value) {
+            if (strpos($key, 'TYPE_') !== false) {
+                $this->getField()->removeClass($value);
+            }
+        }
+
+        $this->getField()->addClass($type);
+
+        return $this;
     }
 
     /**
