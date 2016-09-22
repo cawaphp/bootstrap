b2af64173ffa9a42d83342964bd722a57c8bfd60
tchiotludo <tchiot.ludo@gmail.com>
2016-09-22 15:48:15 +0200

    Breadcrumb : handle schema.org

--- src/Components/Breadcrumb.php
+++ src/Components/Breadcrumb.php
@@ -13,26 +13,51 @@ declare (strict_types=1);
 
 namespace Cawa\Bootstrap\Components;
 
+use Cawa\App\HttpFactory;
 use Cawa\Html\Link;
+use Cawa\Renderer\Container;
 use Cawa\Renderer\HtmlContainer;
 use Cawa\Renderer\HtmlElement;
 
-class Breadcrumb extends HtmlContainer
+class Breadcrumb extends Container
 {
+    use HttpFactory;
+
+    /**
+     * @var HtmlContainer
+     */
+    private $ol;
+
+    /**
+     * @var HtmlElement
+     */
+    private $schema;
+
     /**
      * @param array $links
      */
     public function __construct(array $links = [])
     {
-        parent::__construct('<ol>');
-        $this->addClass('breadcrumb');
+        $this->setOl($links)
+            ->setSchema($links);
+    }
+
+    /**
+     * @param array $links
+     *
+     * @return $this
+     */
+    private function setOl(array $links = []) : self
+    {
+        $this->ol = new HtmlContainer('<ol>');
+        $this->ol->addClass('breadcrumb');
 
         $last = null;
         foreach ($links as $uri => $link) {
             if (!$uri) {
                 $last = $link;
             } else {
-                $this->add(
+                $this->ol->add(
                     (new HtmlContainer('<li>'))
                         ->add(
                             new Link($link, $uri)
@@ -42,10 +67,52 @@ class Breadcrumb extends HtmlContainer
         }
 
         if ($last) {
-            $this->add(
+            $this->ol->add(
                 (new HtmlElement('<li>', $last))
                     ->addClass('active')
             );
         }
+
+        $this->add($this->ol);
+
+        return $this;
     }
+
+
+    /**
+     * @param array $links
+     *
+     * @return $this
+     */
+    private function setSchema(array $links = []) : self
+    {
+        $this->schema = new HtmlContainer('<script>');
+        $this->schema->addAttribute('type', 'application/ld+json');
+
+        $content = [
+            '@context' => "http://schema.org",
+            '@type' => 'BreadcrumbList',
+            'itemListElement' => [],
+        ];
+
+        $uri = clone self::request()->getUri();
+        $count = 1;
+        foreach ($links as $url => $link) {
+            $content['itemListElement'][] = [
+                '@type' => 'ListItem',
+                'position' => $count,
+                'item' => [
+                    '@id' => $uri->setPath($url)->get(false),
+                    'name' => $link,
+                ],
+            ];
+            $count++;
+        }
+
+        $this->schema->setContent(json_encode($content));
+        $this->add($this->schema);
+
+        return $this;
+    }
+
 }
