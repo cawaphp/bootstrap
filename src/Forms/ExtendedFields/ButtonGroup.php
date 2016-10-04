018a7d6ca06f040de5b8812233b794c77c2ea38f
tchiotludo <tchiot.ludo@gmail.com>
2016-10-04 18:57:21 +0200

    Form : ButtonGroup

---
+++ src/Forms/ExtendedFields/ButtonGroup.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Сáша framework.
+ *
+ * (c) tchiotludo <http://github.com/tchiotludo>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Cawa\Bootstrap\Forms\ExtendedFields;
+
+use Cawa\Bootstrap\Forms\Fields\FieldTrait;
+use Cawa\Html\Forms\Fields\AbstractField;
+use Cawa\Html\Forms\FieldsProperties\MultipleTrait;
+use Cawa\Renderer\Container;
+use Cawa\Renderer\HtmlContainer;
+use Cawa\Renderer\HtmlElement;
+
+class ButtonGroup extends AbstractField
+{
+    use FieldTrait {
+        FieldTrait::layout as private fieldTraitLayout;
+    }
+    use MultipleTrait;
+
+    /**
+     * @param string $name
+     * @param string $label
+     * @param array $values
+     */
+    public function __construct(string $name, string $label = null, array $values = [])
+    {
+        parent::__construct('<input />', $name, $label);
+        $this->getField()->addAttribute('type', 'hidden');
+        $this->getField()->setRenderable(false);
+
+        $this->addClass('form-group cawa-fields-button-group');
+
+        $this->values = $values;
+    }
+
+    /**
+     * @var array
+     */
+    private $values;
+
+    /**
+     * @return HtmlContainer
+     */
+    public function getGroup() : HtmlContainer
+    {
+        $container = (new HtmlContainer('<div>'))
+            ->addAttributes([
+                'class' => 'btn-group',
+                'data-toggle' => 'buttons',
+            ]);
+
+        foreach ($this->values as $key => $value) {
+            $container->add($label = (new HtmlContainer('<label>'))
+                ->addClass('btn btn-default')
+                ->add($input = (new HtmlElement('<input>'))
+                    ->addAttributes([
+                        'name' => $this->getName(),
+                        'autocomplete' => 'off',
+                        'value' => $key,
+                        'type' => $this->isMultiple() ? 'checkbox' : 'radio'
+                    ])
+                    ->setContent($value)
+                )
+            );
+
+            if ($this->getValue() == $key ) {
+                $label->addClass('active');
+                $input->addAttribute('checked', 'checked');
+            }
+        }
+
+        return $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function layout(): Container
+    {
+        $container = $this->fieldTraitLayout();
+
+        $add = (new HtmlContainer('<div>'))
+            ->addClass('form-control-static')
+            ->add($this->getGroup());
+
+        if (!$this->getGridSize()) {
+            $container->add($add);
+        } else {
+            foreach ($container->getElements() as $element) {
+                if ($element->getTag() == '<div>') {
+                    $element->add($this->getGroup());
+                }
+            }
+        }
+
+        return $container;
+    }
+
+}
