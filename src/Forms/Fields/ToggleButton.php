b49a807c7f77cca63f83c575bf9f31bea87bd7af
tchiotludo <tchiot.ludo@gmail.com>
2017-01-11 18:29:46 +0100

    Form : Button & ToggleButton

--- src/Forms/Fields/ToggleButton.php
+++ src/Forms/Fields/ToggleButton.php
@@ -13,39 +13,48 @@ declare (strict_types = 1);
 
 namespace Cawa\Bootstrap\Forms\Fields;
 
-class Button extends \Cawa\Html\Forms\Fields\Button
+use Cawa\Html\Forms\Fields\Button;
+use Cawa\Renderer\Container;
+
+class ToggleButton extends Button
 {
     use FieldTrait {
         FieldTrait::layout as private fieldTraitLayout;
     }
 
-    const TYPE_DEFAULT = 'btn-default';
-    const TYPE_PRIMARY = 'btn-primary';
-    const TYPE_SUCCESS = 'btn-success';
-    const TYPE_INFO = 'btn-info';
-    const TYPE_WARNING = 'btn-warning';
-    const TYPE_DANGER = 'btn-danger';
+    const DISPLAYTYPE_DEFAULT = 'btn-default';
+    const DISPLAYTYPE_PRIMARY = 'btn-primary';
+    const DISPLAYTYPE_SUCCESS = 'btn-success';
+    const DISPLAYTYPE_INFO = 'btn-info';
+    const DISPLAYTYPE_WARNING = 'btn-warning';
+    const DISPLAYTYPE_DANGER = 'btn-danger';
 
     /**
      * {@inheritdoc}
      */
-    public function __construct(string $value)
+    public function __construct(string $content, string $label = null)
     {
-        parent::__construct($value);
+        parent::__construct($content, $label);
         $this->getField()->addClass(['btn', 'btn-primary']);
+        $this->getField()->addAttributes([
+            'data-toggle' => 'button',
+            'aria-pressed' => 'false',
+            'autocomplete' => 'off',
+        ]);
+
         $this->addClass('form-group');
     }
 
     /**
      * @param string $type
      *
-     * @return $this|self
+     * @return $this|ToggleButton
      */
     public function setDisplayType(string $type) : self
     {
         $reflection = new \ReflectionClass($this);
         foreach ($reflection->getConstants() as $key => $value) {
-            if (strpos($key, 'TYPE_') !== false) {
+            if (strpos($key, 'DISPLAYTYPE_') !== false) {
                 $this->getField()->removeClass($value);
             }
         }
@@ -53,5 +62,13 @@ class Button extends \Cawa\Html\Forms\Fields\Button
         $this->getField()->addClass($type);
 
         return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function layout() : Container
+    {
+        return self::fieldTraitLayout();
     }
 }
