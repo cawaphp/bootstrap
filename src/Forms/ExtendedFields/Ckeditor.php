515c2c2f02a98a2a754a41e443227ded080cdf49
tchiotludo <tchiot.ludo@gmail.com>
2017-09-20 08:34:01 +0200

    Ckeditor EnterMode

--- src/Forms/ExtendedFields/Ckeditor.php
+++ src/Forms/ExtendedFields/Ckeditor.php
@@ -14,15 +14,54 @@ declare(strict_types = 1);
 namespace Cawa\Bootstrap\Forms\ExtendedFields;
 
 use Cawa\Bootstrap\Forms\Fields\Textarea;
+use Cawa\Renderer\Container;
+use Cawa\Renderer\WidgetOption;
 
 class Ckeditor extends Textarea
 {
+    /**
+     * @var WidgetOption
+     */
+    private $widgetOptions;
+
     /**
      * {@inheritdoc}
      */
     public function __construct(string $name, string $label = null)
     {
+        $this->widgetOptions = new WidgetOption();
+
         parent::__construct($name, $label);
         $this->getField()->addClass('cawa-fields-ckeditor');
+    }
+
+    const ENTER_P = 1;
+    const ENTER_BR = 2;
+    const ENTER_DIV = 3;
+    
+    /**
+     * @param int $mode
+     *
+     * @return $this|self|parent
+     */
+    public function setEnterMode(int $mode = null) : parent
+    {
+        $this->widgetOptions->addData('plugin', ['enterMode' => $mode]);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function layout() : Container
+    {
+        $container = parent::layout();
+        if ($this->widgetOptions->count()) {
+            $instance = $this->getFieldContainer($container);
+            $instance->add($this->widgetOptions);
+        }
+
+        return $container;
     }
 }
